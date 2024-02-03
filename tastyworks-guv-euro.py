6f9dffcbd8127756e1e713b0154cac5460df1a9b
konrad <konrad.scham@gmail.com>
2024-02-03 14:38:52 +0100

    reading csv

--- tastyworks-guv-euro.py
+++ tastyworks-guv-euro.py
@@ -9,20 +9,23 @@
 import sys
 import os
 import datetime as pydatetime
-import pandas
+from typing import List
 
+import pandas
+from pandas import DataFrame
 
 eurkurs = None
-eurusd_csv
-#euro_ezb_url: str = 'https://www.bundesbank.de/statistic-rmi/StatisticDownload?tsId=BBEX3.D.USD.EUR.BB.AC.000&its_csvFormat=en&its_fileFormat=csv&mode=its&its_from=2010'
+
+
+# euro_ezb_url: str = 'https://www.bundesbank.de/statistic-rmi/StatisticDownload?tsId=BBEX3.D.USD.EUR.BB.AC.000&its_csvFormat=en&its_fileFormat=csv&mode=its&its_from=2010'
 
 # Setup 'eurkurs' as dict() to contain the EURUSD exchange rate on a given date
 # based on official data from ezb.de.
 # If the file 'eurusd.csv' does not exist, download the data from
 # the bundesbank directly.
-def read_eurusd() -> None:
+def read_eurusd(eurusd_csv: str) -> None:
     import csv
-    global eurkurs,eurusd_csv
+    global eurkurs
 
     if not os.path.exists(eurusd_csv):
         raise 'eurousd_csv does not exist'
@@ -38,29 +41,62 @@ def read_eurusd() -> None:
                 else:
                     eurkurs[date] = None
 
+
 def get_eurusd(date: str) -> float:
     while True:
         try:
             x = eurkurs[date]
         except KeyError:
             print(f'ERROR: No EURUSD conversion data available for {date},'
-                ' please download newer data into the file eurusd.csv.')
+                  ' please download newer data into the file eurusd.csv.')
             sys.exit(1)
         if x is not None:
             return x
         date = str(pydatetime.date(*map(int, date.split('-'))) - pydatetime.timedelta(days=1))
 
+
+# check if the first line of the csv line contains the correct header:
+def check_csv(csv_file) -> None:
+    with open(csv_file, encoding='UTF8') as f:
+        content = f.readlines()
+    if len(content) < 1 or content[
+        0] != 'TAX_YEAR,SUBLOT_ID,SECNO,CUSIP,SYMBOL,SEC_DESCR,SEC_TYPE,SEC_SUBTYPE,SUBACCOUNT_TYPE,OPEN_TRAN_ID,CLOSE_TRAN_ID-SEQNO,OPEN_DATE,CLOSE_DATE,CLOSE_EVENT,DISPOSAL_METHOD,QUANTITY,LONG_SHORT_IND,NO_WS_COST,NO_WS_PROCEEDS,NO_WS_GAINLOSS,WS_COST_ADJ,WS_PROC_ADJ,WS_LOSS_ID-SEQNO,1099_ACQ_DATE,1099_DISP_DATE,1099_COST,1099_PROCEEDS,GROSS_NET_IND,TOTAL_GAINLOSS,ORDINARY_GAINLOSS,1099_DISALLOWED_LOSS,1099_MARKET_DISCOUNT,8949_GAINLOSS,8949_CODE,HOLDING_DATE,TERM,COVERED_IND,8949_BOX,1099_1256_CY_REALIZED,1099_1256_PY_UNREALIZED,1099_1256_CY_UNREALIZED,1099_1256_AGGREGATE\n':
+        print('ERROR: Wrong first line in csv file. Please download trade history from the web page.')
+        sys.exit(1)
+
+
+def read_csv_tasty(csv_file: str) -> pandas.DataFrame:
+    """
+
+    :type csv_file: str
+    """
+    check_csv(csv_file)
+    wk = pandas.read_csv(csv_file, parse_dates=['CLOSE_DATE'])
+    return wk
+
+
 def usage() -> None:
     print('tastyworks-guv-euro.py [-ouv]' +
-        '[--output=test.csv][--eurusd=eurusd.csv][--help]' +
-        '[--verbose][--debug][--show] *.csv')
+          '[--output=test.csv][--eurusd=eurusd.csv][--help]' +
+          '[--verbose][--debug][--show] *.csv')
+
+
+def augmenteuramount(dataframe:pandas.DataFrame):
+    for i in dataframe.index:
+        if debug:
+            print(dataframe.iloc[i].to_string())
+        (datetime, tcode, tsubcode, symbol, buysell, openclose, quantity, expire, strike,
+            callput, price, fees, amount, description, account_ref) = wk.iloc[i]
+
 
 def main(argv) -> None:
+    eurusd_csv: str = "https://www.bundesbank.de/statistic-rmi/StatisticDownload?tsId=BBEX3.D.USD.EUR.BB.AC.000&its_csvFormat=en&its_fileFormat=csv&mode=its&its_from=2010"
     try:
         import getopt
         opts, args = getopt.getopt(argv, 'o:u:vhd', ['eurusd=',
-            'help', 'output=',
-            'verbose', 'debug'])
+                                                     'help', 'output=',
+                                                     'verbose', 'debug'])
+
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -90,9 +126,10 @@ def main(argv) -> None:
         sys.exit()
     read_eurusd(eurusd_csv)
     args.reverse()
-    all_wk = []
+    dataframe: DataFrame
     for csv_file in args:
-        all_wk.append(read_csv_tasty(csv_file))
+        dataframe = read_csv_tasty(csv_file)
+    augmenteuramount(dataframe)
 
 
 # Press the green button in the gutter to run the script.
